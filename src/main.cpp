0b754646cba9ffab616cb79b9ad78cb7e8a2efb4
devsamoyo <samoyo@protonmail.com>
2021-10-04 18:20:54 -0400

    Update main.cpp: Update comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,17 @@
 #include "main.hpp"
 
 int main(int argc, char **argv) {
-    // get hostname from args
     if (argc < 2) {
         fprintf(stderr, "USAGE: %s <HOSTNAME> [HOSTNAME...]\nie: %s www.example.com\n", argv[0], argv[0]);
         exit(EXIT_FAILURE);
     }
 
+    // for every hostname in argv
     for (int i = 1; i < argc; i++) {
         char *host = argv[i];
         struct hostent *hostn = gethostbyname(host);
         
+        // <hostname>:\t<ip>
         std::cout << hostn->h_name << ":\t" << strdup(inet_ntoa(
             *((struct in_addr*)hostn->h_addr_list[0])
         )) << std::endl;
