959852c46a99d7a599460dc7444c15aefc1f2c5c
devsamoyo <samoyo@protonmail.com>
2021-10-04 18:19:20 -0400

    Update main.hpp: Remove Unnessecary Dependencies

--- src/main.hpp
+++ src/main.hpp
@@ -1,10 +1,6 @@
 #pragma once
 
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <netdb.h>
-#include <string>
 #include <iostream>
-#include <stdio.h>
 #include <memory.h>
 #include <arpa/inet.h>
